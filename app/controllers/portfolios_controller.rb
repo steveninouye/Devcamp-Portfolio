e21b519c84c9011308c90d8bbc383c5d090b48e9
Steven Inouye <steveninouye@msn.com>
2018-11-04 20:03:10 -0800

    Implement show functionality for portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,38 +1,43 @@
 class PortfoliosController < ApplicationController
-    def index
-        @portfolio_items = Portfolio.all
-    end
-    
-    def new
-        @portfolio_item = Portfolio.new
-    end
+  def index
+    @portfolio_items = Portfolio.all
+  end
 
-    def create
-        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-    
-        respond_to do |format|
-          if @portfolio_item.save
-            format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
-          else
-            format.html { render :new }
-          end
-        end
-    end
+  def new
+    @portfolio_item = Portfolio.new
+  end
 
-    def edit
-        @portfolio_item = Portfolio.find(params[:id])
-    end
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
-    def update
-        @portfolio_item = Portfolio.find(params[:id])
-
-        respond_to do |format|
-            if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-            format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
-            else
-            format.html { render :edit }
-            end
-        end
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
     end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+      format.html { render :edit }
+      end
+    end
+  end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+
+  end
 
 end
